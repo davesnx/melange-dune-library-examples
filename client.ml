7fdad46963ad9b349c2a1f180556d8ed333dcc87
David Sancho Moreno <dsnxmoreno@gmail.com>
2025-07-28 17:42:02 +0200

    Pretty output

--- client.ml
+++ client.ml
@@ -1,8 +1,8 @@
 let () =
   let url = "https://sancho.dev" in
-  Printf.printf "input is self closing? %b" (Pure_library.Html.is_self_closing_tag "input");
-  Printf.printf "%s" (URL.toString (URL.makeExn url));
-  Printf.printf "%s" (Library_using_a_shared_library.Main.scream url);
+  Printf.printf "input is self closing? %b\n" (Pure_library.Html.is_self_closing_tag "input");
+  Printf.printf "%s\n" (URL.toString (URL.makeExn url));
+  Printf.printf "%s\n" (Library_using_a_shared_library.Main.scream url);
   let promise = Promise.resolved 23 in
   let result = Promise.map promise (fun x -> x + 1) in
-  Promise.get result (fun _ -> Printf.printf "done\n")
+  Promise.get result (fun int -> Printf.printf "done: %d\n" int)
