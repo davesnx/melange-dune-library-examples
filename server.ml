e9dcc65e5f9c4b57fdc4b4b8503b4b4310b0bdbc
David Sancho Moreno <dsnxmoreno@gmail.com>
2025-07-28 17:54:02 +0200

    Run callbacks

--- server.ml
+++ server.ml
@@ -5,4 +5,10 @@ let () =
   Printf.printf "%s\n" (Library_using_a_shared_library.Main.scream url);
   let promise = Promise.resolved 23 in
   let result = Promise.map promise (fun x -> x + 1) in
-  Promise.get result (fun _ -> Printf.printf "done\n")
+  Promise.get result (fun r -> Printf.printf "done: %d\n" r);
+
+  (* Process pending callbacks since native doesn't have event loop *)
+  while Promise.ReadyCallbacks.callbacksPending () do
+    let snapshot = Promise.ReadyCallbacks.snapshot () in
+    Promise.ReadyCallbacks.call snapshot
+  done
