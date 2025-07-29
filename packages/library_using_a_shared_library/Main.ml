f1d1b78e20b1cbb104fa9daeb6edb1cd9e769fdb
David Sancho Moreno <dsnxmoreno@gmail.com>
2025-07-29 14:21:10 +0200

    Make library using shared libs, depend on all of them

--- packages/library_using_a_shared_library/Main.ml
+++ packages/library_using_a_shared_library/Main.ml
@@ -1,3 +1,3 @@
 let scream str =
-  let uppercased = str |> URL.makeExn |> URL.toString |> String.uppercase_ascii in
+  let uppercased = str |> URL.make |> Option.get |> URL.host |> Option.get |> String.uppercase_ascii in
   uppercased ^ "!!!"
